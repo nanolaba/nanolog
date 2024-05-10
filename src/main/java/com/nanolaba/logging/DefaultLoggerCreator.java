a076db4aaa9d439cbdee79496847511ba844dd64
alexander.andriishin <alexander@andriishin.com>
2024-05-10 13:57:59 +0300

    added configuration option using config file

--- src/main/java/com/nanolaba/logging/DefaultLoggerCreator.java
+++ src/main/java/com/nanolaba/logging/DefaultLoggerCreator.java
@@ -1,22 +1,54 @@
 package com.nanolaba.logging;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DefaultLoggerCreator {
 
-    public static final String LOGGER_CLASS_PROPERTY = "nanolog.logger";
+    public static final String PROPERTY_LOGGER_CLASS = "nanolog.logger";
+    public static final String PROPERTY_LOGGER_CONFIG_FILE = "nanolog.config";
+
+    public static final String DEFAULT_CONFIGURATION_FILE = "nanolog.properties";
+
+    private DefaultLoggerCreator() {
+    }
 
     public static ILogger createDefaultLogger() {
 
-        String property = System.getProperty(LOGGER_CLASS_PROPERTY);
-        if (property != null && !property.isEmpty()) {
+        loadPropertiesFromFile();
+
+        String logger = System.getProperty(PROPERTY_LOGGER_CLASS);
+        if (logger != null && !logger.isEmpty()) {
             try {
-                Class<?> loggerClass = Class.forName(property);
+                Class<?> loggerClass = Class.forName(logger);
                 return (ILogger) loggerClass.getConstructor().newInstance();
             } catch (Throwable e) {
-                System.err.printf("[NANOLOG] Property \"%s\" has been defined, but an object of class \"%s\" cannot be created %n", LOGGER_CLASS_PROPERTY, property);
+                System.err.printf("[NANOLOG] Property \"%s\" has been defined, but an object of class \"%s\" cannot be created %n", PROPERTY_LOGGER_CLASS, logger);
                 e.printStackTrace(System.err);
             }
         }
 
         return new ConsoleLogger();
+    }
+
+    private static void loadPropertiesFromFile() {
+        String config = System.getProperty(PROPERTY_LOGGER_CONFIG_FILE, DEFAULT_CONFIGURATION_FILE);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        if (classLoader.getResource(config) != null) {
+            try (InputStream stream = classLoader.getResourceAsStream(config)) {
+                Properties props = new Properties();
+                props.load(stream);
+                props.forEach((name, value) -> {
+                    if (System.getProperty((String) name) == null) {
+                        System.setProperty((String) name, (String) value);
+                    }
+                });
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (!config.equals(DEFAULT_CONFIGURATION_FILE)) {
+            System.err.printf("[NANOLOG] Property \"%s\" has been defined, but the file \"%s\" cannot be found %n", DEFAULT_CONFIGURATION_FILE, config);
+        }
     }
 }
--- src/test/java/com/nanolaba/logging/DefaultLoggerCreatorTest.java
+++ src/test/java/com/nanolaba/logging/DefaultLoggerCreatorTest.java
@@ -1,28 +1,72 @@
 package com.nanolaba.logging;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import static com.nanolaba.logging.DefaultLoggerCreator.PROPERTY_LOGGER_CLASS;
+import static com.nanolaba.logging.DefaultLoggerCreator.PROPERTY_LOGGER_CONFIG_FILE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DefaultLoggerCreatorTest extends AbstractLoggerTest {
 
+    @AfterEach
+    public void clearProperties() {
+        System.clearProperty(PROPERTY_LOGGER_CONFIG_FILE);
+        System.clearProperty(PROPERTY_LOGGER_CLASS);
+    }
+
     @Test
     public void systemPropertyTest() {
-        System.setProperty(DefaultLoggerCreator.LOGGER_CLASS_PROPERTY, Slf4jLogger.class.getName());
+        System.setProperty(PROPERTY_LOGGER_CLASS, Slf4jLogger.class.getName());
         LOG.init();
         assertEquals(LOG.getLogger().getClass(), Slf4jLogger.class);
 
-        System.clearProperty(DefaultLoggerCreator.LOGGER_CLASS_PROPERTY);
+        System.clearProperty(PROPERTY_LOGGER_CLASS);
         LOG.init();
         assertEquals(LOG.getLogger().getClass(), ConsoleLogger.class);
     }
 
     @Test
     public void systemPropertyFailTest() {
-        System.setProperty(DefaultLoggerCreator.LOGGER_CLASS_PROPERTY, "Unexisted class");
+        System.setProperty(PROPERTY_LOGGER_CLASS, "Unexisted class");
         LOG.init();
+
         assertEquals(LOG.getLogger().getClass(), ConsoleLogger.class);
         assertTrue(getErrAndClear().contains("java.lang.ClassNotFoundException: Unexisted class"));
+    }
+
+    @Test
+    public void filePropertyTest() {
+        System.setProperty(PROPERTY_LOGGER_CONFIG_FILE, "test.properties");
+        LOG.init();
+        assertEquals(Slf4jLogger.class, LOG.getLogger().getClass());
+
+        System.clearProperty(PROPERTY_LOGGER_CONFIG_FILE);
+        System.clearProperty(PROPERTY_LOGGER_CLASS);
+        LOG.init();
+        assertEquals(ConsoleLogger.class, LOG.getLogger().getClass());
+    }
+
+    @Test
+    public void existingPropertyIsNotOverwrittenByTheFileTest() {
+        System.setProperty(PROPERTY_LOGGER_CLASS, ConsoleLogger.class.getName());
+        System.setProperty(PROPERTY_LOGGER_CONFIG_FILE, "test.properties");
+        LOG.init();
+        assertEquals(ConsoleLogger.class, LOG.getLogger().getClass());
+
+        System.clearProperty(PROPERTY_LOGGER_CONFIG_FILE);
+        System.clearProperty(PROPERTY_LOGGER_CLASS);
+        LOG.init();
+        assertEquals(ConsoleLogger.class, LOG.getLogger().getClass());
+    }
+
+    @Test
+    public void unexistedFilePropertyFailTest() {
+        System.setProperty(PROPERTY_LOGGER_CONFIG_FILE, "unexisted.properties");
+        LOG.init();
+
+        assertEquals(LOG.getLogger().getClass(), ConsoleLogger.class);
+        assertTrue(getErrAndClear().contains("[NANOLOG] Property \"nanolog.properties\" has been defined, but the file \"unexisted.properties\" cannot be found"));
     }
 }
